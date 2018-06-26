d654f1209e4da50190330c432c7007457a7cf5fc
Unknown <dimidyuk9897@gmail.com>
2018-06-27 00:09:03 +0300

    Day1

---
+++ NET.S.2018.Dimidyuk.01/OuickSort.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OuickSort.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OuickSort.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("83f03986-a145-4dba-9f00-24a0425d73b1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ NET.S.2018.Dimidyuk.01/QuickSort/QuickSort.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace QuickSort
+{
+
+    public static class QuickSort
+    {
+
+        public static int[] TheQuickSort(this int[] array)
+        {
+
+            if (array == null)
+            {
+                throw new ArgumentNullException($"The value of parameter{nameof(array)} can't be null");
+            }
+
+            if (array.Length == 0)
+            {
+                throw new ArgumentException($"The total length of {nameof(array)} can't be 0");
+            }
+
+            return Quicksort(array, 0, array.Length - 1);
+        }
+
+        public static int[] Quicksort(int[] array, int left, int right)
+        {
+
+            int l = left, r = right;
+
+            int pivot = array[(left + right) / 2];
+
+            while (l <= r)
+            {
+
+                while (array[l].CompareTo(pivot) < 0)
+                {
+                    l++;
+                }
+
+                while (array[r].CompareTo(pivot) > 0)
+                {
+                    r--;
+                }
+
+                if (l <= r)
+                {
+                    Swap(ref array[l], ref array[r]);
+                    l++;
+                    r--;
+                }
+            }
+
+            if (left < r)
+            {
+                Quicksort(array, left, r);
+            }
+
+            if (l < right)
+            {
+                Quicksort(array, l, right);
+            }
+
+            return array;
+        }
+
+        private static void Swap(ref int a, ref int b)
+        {
+            var tmp = a;
+            a = b;
+            b = tmp;
+        }
+    }
+}
