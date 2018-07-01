325f2bc61a56b1743c536a8a5ba09c9279f6ba9f
Unknown <dimidyuk9897@gmail.com>
2018-07-01 16:21:11 +0300

    Day3

---
+++ NET.S.2018.Dimidyuk.03/#1/FindNthRoot.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace FindNthRoot
+{
+    public class FindNthRoot
+    {
+        #region NewtonMethod
+        /// <summary>
+        /// Method for finding the root of n degree of a number with a given accuracy.
+        /// </summary>
+        /// <param name="number">Number for taking a root.</param>
+        /// <param name="n">Degree.</param>
+        /// <param name="accuracy">Calculation accuracy.</param>
+        /// <returns></returns>
+        public static double NewtonMethod(double number, double n, double accuracy)
+        {
+            CheckData(number, n, accuracy);
+
+            double x0 = number / n;
+            double x1 = (1 / n) * ((n - 1) * x0 + (number / Math.Pow(x0, n - 1)));
+
+            while (Math.Abs(x1 - x0) > accuracy)
+            {
+                x0 = x1;
+                x1 = (1 / n) * ((n - 1) * x0 + (number / Math.Pow(x0, n - 1)));
+            }
+
+            return x1;
+        }
+        #endregion
+
+        #region CheckData
+        ///<summary>
+        ///Method for checking given data in method
+        ///</summary>
+        ///<param name="number">Number for taking a root.</param>
+        ///<param name="n">Degree.</param>
+        /// <param name="accuracy">Calculation accuracy.</param>
+        /// <returns></returns>
+        private static void CheckData(double number, double n, double accuracy)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException($"Incorrect input of {nameof(number)}.");
+            }
+
+            if (n < 0)
+            {
+                throw new ArgumentException($"Incorrect input of {nameof(n)}.");
+            }
+
+            if (accuracy < 0)
+            {
+                throw new ArgumentException($"Incorrect input of {nameof(accuracy)}.");
+            }
+        }
+        #endregion
+    }
+}
---
+++ NET.S.2018.Dimidyuk.03/#1/FindNthRootTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using FindNthRoot;
+using System;
+
+namespace FindNthRoot.Tests
+{
+    [TestClass]
+    public class FindNthRootTests
+    {
+        [TestCase(1, 5, 0.0001,  1)]
+        [TestCase(8, 3, 0.0001,  2)]
+        [TestCase(0.001, 3, 0.0001, 0.1)]
+        [TestCase(0.04100625, 4, 0.0001, 0.45)]
+        [TestCase(8, 3, 0.0001,  2)]
+        [TestCase(0.0279936, 7, 0.0001,  0.6)]
+        [TestCase(0.004241979, 9, 0.00000001, 0.545)]
+        public void NewtonMethod_CorrectInputValues_PositiveTest(double number, double n, double accuracy, double ExpectedResult)
+        {
+            double actual = FindNthRoot.NewtonMethod(number, n, accuracy);
+            double expected = ExpectedResult;
+            NUnit.Framework.Assert.AreEqual(actual, expected, accuracy);
+        }
+        [TestCase(-0.01, 2, 0.0001)]
+        [TestCase(0.01, -2, 0.0001)]
+        [TestCase(0.01, 2, -0.0001)]
+        public void Insertion_InputValuesLess0_ThrowsArgumentException(double number, double n, double accuracy)
+        {
+            NUnit.Framework.Assert.Throws<ArgumentException>(() => FindNthRoot.NewtonMethod(number, n, accuracy));
+        }
+
+    }
+}
