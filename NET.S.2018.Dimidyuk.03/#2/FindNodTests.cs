a1ff2b2b8e963b7f19fe29877e700c9fb48e9df5
Unknown <dimidyuk9897@gmail.com>
2018-07-07 16:32:54 +0300

    Added StyleCop, methods for three elements

--- NET.S.2018.Dimidyuk.03/#2/FindNodTests.cs
+++ NET.S.2018.Dimidyuk.03/#2/FindNodTests.cs
@@ -18,6 +18,18 @@ namespace FindNod.Tests
         }
         #endregion
 
+        #region EuclidForThree
+        [TestCase(1, 10, 11, ExpectedResult = 1)]
+        [TestCase(5, 10, 13, ExpectedResult = 1)]
+        [TestCase(24, 24, 24, ExpectedResult = 24)]
+        [TestCase(-5, 10, 15, ExpectedResult = 5)]
+        [TestCase(5, 0, 2, ExpectedResult = 1)]
+        public int FindNod_EuclidMethodForThree_CorrectInputValues_PositiveTest(int first, int second, int third)
+        {
+            return Nod.EuclidMethod(first, second, third);
+        }
+        #endregion
+
         #region EuclidForArray
         [TestCase(new int[] {5, 10, 15, 20}, ExpectedResult = 5)]
         [TestCase(new int[] { 5, 0, 15, -20 }, ExpectedResult = 5)]
@@ -37,6 +49,18 @@ namespace FindNod.Tests
         public int FindNod_SteinMethodForTwo_CorrectInputValues_PositiveTest(int first, int second)
         {
             return Nod.EuclidMethod(first, second);
+        }
+        #endregion
+
+        #region SteinForThree
+        [TestCase(1, 10, 11, ExpectedResult = 1)]
+        [TestCase(5, 10, 13, ExpectedResult = 1)]
+        [TestCase(24, 24,24, ExpectedResult = 24)]
+        [TestCase(-5, 10, 15, ExpectedResult = 5)]
+        [TestCase(5, 0, 2, ExpectedResult = 1)]
+        public int FindNod_SteinMethodForThree_CorrectInputValues_PositiveTest(int first, int second, int third)
+        {
+            return Nod.SteinMethod(first, second, third);
         }
         #endregion
 
--- NET.S.2018.Dimidyuk.03/#2/Nod.cs
+++ NET.S.2018.Dimidyuk.03/#2/Nod.cs
@@ -2,13 +2,16 @@
 
 namespace FindNod
 {
+    /// <summary>
+    /// Finds NOD of 2,3 and array of numbers
+    /// using Euclid and Stein methods
+    /// </summary>
     public class Nod
     {
         /// <summary>
         /// Finds NOD using Euclid method for array of parameters.
         /// </summary>
-        /// <param name="first">Left int parameter.</param>
-        /// <param name="second">Right int parameter.</param>
+        /// <param name="array">The array.</param>
         /// <returns>NOD of all parameters.</returns>
         public static int EuclidMethod(params int[] array)
         {
@@ -24,18 +27,40 @@ namespace FindNod
         }
 
         /// <summary>
+        /// Finds NOD using Euclid method for three parameters.
+        /// </summary>
+        /// <param name="first">The first number.</param>
+        /// <param name="second">The second number.</param>
+        /// <param name="third">The third number.</param>
+        /// <returns>NOD of three parameters</returns>
+        public static int EuclidMethod(int first, int second, int third)
+        {
+            int nod = EuclidMethod(first, second);
+            nod = EuclidMethod(third, nod);
+
+            return nod;
+        }
+
+        /// <summary>
         /// Finds NOD using Euclid method for 2 parameters.
         /// </summary>
-        /// <param name="first">Left int parameter.</param>
-        /// <param name="second">Right int parameter.</param>
-        /// <returns>NOD of all parameters.</returns>
+        /// <param name="first">Left parameter.</param>
+        /// <param name="second">Right parameter.</param>
+        /// <returns>NOD of two parameters.</returns>
         public static int EuclidMethod(int first, int second)
         {
             first = Math.Abs(first);
             second = Math.Abs(second);
 
-            if (first == 0) return second;
-            if (second == 0) return first;
+            if (first == 0)
+            {
+                return second;
+            }
+
+            if (second == 0)
+            {
+                return first;
+            }
 
             while (first != second)
             {
@@ -54,8 +79,7 @@ namespace FindNod
         /// <summary>
         /// Finds NOD using Stein method for array of parameters.
         /// </summary>
-        /// <param name="first">Left int parameter.</param>
-        /// <param name="second">Right int parameter.</param>
+        /// <param name="array">The array.</param>
         /// <returns>NOD of all parameters.</returns>
         public static int SteinMethod(params int[] array)
         {
@@ -71,53 +95,104 @@ namespace FindNod
         }
 
         /// <summary>
+        /// Finds NOD using Stein method for three parameters.
+        /// </summary>
+        /// <param name="first">The first parameter.</param>
+        /// <param name="second">The second parameter.</param>
+        /// <param name="third">The third parameter.</param>
+        /// <returns>NOD of three parameters</returns>
+        public static int SteinMethod(int first, int second, int third)
+        {
+            int nod = SteinMethod(first, second);
+            nod = SteinMethod(third, nod);
+
+            return nod;
+        }
+
+        /// <summary>
         /// Finds NOD using Stein method for 2 parameters.
         /// </summary>
-        /// <param name="first">Left int parameter.</param>
-        /// <param name="second">Right int parameter.</param>
-        /// <returns>NOD of all parameters.</returns>
+        /// <param name="first">The left number.</param>
+        /// <param name="second">Right parameter.</param>
+        /// <returns>NOD of two parameters.</returns>
         public static int SteinMethod(int first, int second)
         {
             first = Math.Abs(first);
             second = Math.Abs(second);
 
-            if (first == second) return first;
-            if (first == 0) return second;
-            if (second == 0) return first;
-            if (first == 1 || second == 1) return 1;
+            if (first == second)
+            {
+                return first;
+            }
+
+            if (first == 0)
+            {
+                return second;
+            }
+
+            if (second == 0)
+            {
+                return first;
+            }
+
+            if (first == 1 || second == 1)
+            {
+                return 1;
+            }
 
             if ((first & 1) == 0)
             {
                 if ((second & 1) == 1)
+                {
                     return SteinMethod(first >> 1, second);
+                }
                 else
-                    return (SteinMethod(first >> 1, second >> 1) << 1);
+                {
+                    return SteinMethod(first >> 1, second >> 1) << 1;
+                }
             }
             else
             {
                 if ((second & 1) == 0)
+                {
                     return SteinMethod(first, second >> 1);
+                }
                 else
                 {
                     if (first < second)
+                    {
                         return SteinMethod(first, Math.Abs(first - second) >> 1);
+                    }
                     else
+                    {
                         return SteinMethod(Math.Abs(first - second) >> 1, second);
+                    }
                 }
             }
         }
 
         /// <summary>
-        /// Method checks if for array is inputed correctly.
+        /// Checks the array.
         /// </summary>
-        /// <param name="array">Array, which will be checked.</param>
-        /// <returns>True, if array is inputed correctly, and false otherwise.</returns>
+        /// <param name="array">The array.</param>
+        /// <exception cref="ArgumentNullException">array</exception>
+        /// <exception cref="ArgumentException">array</exception>
         private static void CheckArray(int[] array)
         {
-            if (array is null) throw new ArgumentNullException($"{nameof(array)} is null.");
-            if (array.Length == 0) throw new ArgumentException($"{nameof(array)} is empty.");
-            if (array.Length == 1) throw new ArgumentException($"{nameof(array)} has to take at least 2 elements.");
-        }
+            if (array is null)
+            {
+                throw new ArgumentNullException($"{nameof(array)} is null.");
+            }
 
+            if (array.Length == 0)
+            {
+                throw new ArgumentException($"{nameof(array)} is empty.");
+            }
+
+            if (array.Length == 1)
+            {
+                throw new ArgumentException($"{nameof(array)} has to take at least 2 elements.");
+            }
+        }
     }
 }
