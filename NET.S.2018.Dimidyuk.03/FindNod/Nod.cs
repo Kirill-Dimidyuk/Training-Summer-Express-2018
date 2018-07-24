5bc7f55d46a500b642e1f319de68f209a20e74b6
Unknown <dimidyuk9897@gmail.com>
2018-07-24 18:42:20 +0300

    Updated ( Added delegate)

---
+++ NET.S.2018.Dimidyuk.03/FindNod/Nod.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nod
+{
+    public class Nod
+    {
+        private delegate int FindNod(int first, int second);
+
+        #region Euclid Methods
+        /// <summary>
+        /// Finds NOD using Euclid method for array of parameters.
+        /// </summary>
+        /// <param name="array">The array.</param>
+        /// <returns>NOD of all parameters.</returns>
+        public static int Euclid(params int[] array) => FindNodMethod(Euclid, array);
+
+        /// <summary>
+        /// Finds NOD using Euclid method for three parameters.
+        /// </summary>
+        /// <param name="first">The first number.</param>
+        /// <param name="second">The second number.</param>
+        /// <param name="third">The third number.</param>
+        /// <returns>NOD of three parameters</returns>
+        public static int Euclid(int first, int second, int third) => FindNodMethod(Euclid, first, second, third);
+
+        /// <summary>
+        /// Finds NOD using Euclid method for 2 parameters.
+        /// </summary>
+        /// <param name="first">Left parameter.</param>
+        /// <param name="second">Right parameter.</param>
+        /// <returns>NOD of two parameters.</returns>
+        public static int Euclid(int first, int second) => FindNodMethod(EuclidMethod, first, second);
+        #endregion
+
+        #region Stein Methods
+        /// <summary>
+        /// Finds NOD using Stein method for array of parameters.
+        /// </summary>
+        /// <param name="array">The array.</param>
+        /// <returns>NOD of all parameters.</returns>
+        public static int Stein(params int[] array) => FindNodMethod(SteinMethod, array);
+
+        /// <summary>
+        /// Finds NOD using Stein method for three parameters.
+        /// </summary>
+        /// <param name="first">The first parameter.</param>
+        /// <param name="second">The second parameter.</param>
+        /// <param name="third">The third parameter.</param>
+        /// <returns>NOD of three parameters</returns>
+        public static int Stein(int first, int second, int third) => FindNodMethod(SteinMethod, first, second, third);
+
+        /// <summary>
+        /// Finds NOD using Stein method for 2 parameters.
+        /// </summary>
+        /// <param name="first">The left number.</param>
+        /// <param name="second">Right parameter.</param>
+        /// <returns>NOD of two parameters.</returns>
+        public static int Stein(int first, int second) => FindNodMethod(SteinMethod, first, second);
+        
+        #endregion
+
+        #region Private Methods
+        private static int FindNodMethod(FindNod find, int first, int second)
+        {
+            return find(first, second);
+        }
+
+        private static int FindNodMethod(FindNod find, int first, int second, int third)
+        {
+            return find(third, find(first, second));
+        }
+
+        private static int FindNodMethod(FindNod find, params int[] array)
+        {
+            CheckArray(array);
+
+            int nod = find(array[0], array[1]);
+
+            for (int i = 2; i < array.Length; i++)
+            {
+                nod = find(array[i], nod);
+            }
+
+            return nod;
+        }
+
+        private static int EuclidMethod(int first, int second)
+        {
+            first = Math.Abs(first);
+            second = Math.Abs(second);
+
+            if (first == 0)
+            {
+                return second;
+            }
+
+            if (second == 0)
+            {
+                return first;
+            }
+
+            while (first != second)
+            {
+                if (first > second)
+                {
+                    first -= second;
+                }
+                else
+                {
+                    second -= first;
+                }
+            }
+            return first;
+        }
+
+        private static int SteinMethod(int first, int second)
+        {
+            first = Math.Abs(first);
+            second = Math.Abs(second);
+
+            if (first == second)
+            {
+                return first;
+            }
+
+            if (first == 0)
+            {
+                return second;
+            }
+
+            if (second == 0)
+            {
+                return first;
+            }
+
+            if (first == 1 || second == 1)
+            {
+                return 1;
+            }
+
+            if ((first & 1) == 0)
+            {
+                if ((second & 1) == 1)
+                {
+                    return SteinMethod(first >> 1, second);
+                }
+                else
+                {
+                    return SteinMethod(first >> 1, second >> 1) << 1;
+                }
+            }
+            else
+            {
+                if ((second & 1) == 0)
+                {
+                    return SteinMethod(first, second >> 1);
+                }
+                else
+                {
+                    if (first < second)
+                    {
+                        return SteinMethod(first, Math.Abs(first - second) >> 1);
+                    }
+                    else
+                    {
+                        return SteinMethod(Math.Abs(first - second) >> 1, second);
+                    }
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Checks the array.
+        /// </summary>
+        /// <param name="array">The array.</param>
+        /// <exception cref="ArgumentNullException">array</exception>
+        /// <exception cref="ArgumentException">array</exception>
+        private static void CheckArray(int[] array)
+        {
+            if (array is null)
+            {
+                throw new ArgumentNullException($"{nameof(array)} is null.");
+            }
+
+            if (array.Length == 0)
+            {
+                throw new ArgumentException($"{nameof(array)} is empty.");
+            }
+
+            if (array.Length == 1)
+            {
+                throw new ArgumentException($"{nameof(array)} has to take at least 2 elements.");
+            }
+        }
+        #endregion
+    }
+}
---
+++ NET.S.2018.Dimidyuk.03/FindNod/NodTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace Nod.Tests
+{
+    [TestClass]
+    public class NodTests
+    {
+        #region EuclidForTwo
+        [TestCase(1, 10, ExpectedResult = 1)]
+        [TestCase(5, 10, ExpectedResult = 5)]
+        [TestCase(24, 24, ExpectedResult = 24)]
+        [TestCase(-5, 10, ExpectedResult = 5)]
+        [TestCase(5, 0, ExpectedResult = 5)]
+        public int FindNod_EuclidMethodForTwo_CorrectInputValues_PositiveTest(int first, int second)
+        {
+            return Nod.Euclid(first, second);
+        }
+        #endregion
+
+        #region EuclidForThree
+        [TestCase(1, 10, 11, ExpectedResult = 1)]
+        [TestCase(5, 10, 13, ExpectedResult = 1)]
+        [TestCase(24, 24, 24, ExpectedResult = 24)]
+        [TestCase(-5, 10, 15, ExpectedResult = 5)]
+        [TestCase(5, 0, 2, ExpectedResult = 1)]
+        public int FindNod_EuclidMethodForThree_CorrectInputValues_PositiveTest(int first, int second, int third)
+        {
+            return Nod.Euclid(first, second, third);
+        }
+        #endregion
+
+        #region EuclidForArray
+        [TestCase(new int[] { 5, 10, 15, 20 }, ExpectedResult = 5)]
+        [TestCase(new int[] { 5, 0, 15, -20 }, ExpectedResult = 5)]
+        [TestCase(new int[] { 5, -5, 15, 20 }, ExpectedResult = 5)]
+        public int FindNod_EuclidMethodForArray_CorrectInputValues_PositiveTest(int[] array)
+        {
+            return Nod.Euclid(array);
+        }
+        #endregion
+
+        #region SteinForTwo
+        [TestCase(1, 10, ExpectedResult = 1)]
+        [TestCase(5, 10, ExpectedResult = 5)]
+        [TestCase(24, 24, ExpectedResult = 24)]
+        [TestCase(-5, 10, ExpectedResult = 5)]
+        [TestCase(5, 0, ExpectedResult = 5)]
+        public int FindNod_SteinMethodForTwo_CorrectInputValues_PositiveTest(int first, int second)
+        {
+            return Nod.Stein(first, second);
+        }
+        #endregion
+
+        #region SteinForThree
+        [TestCase(1, 10, 11, ExpectedResult = 1)]
+        [TestCase(5, 10, 13, ExpectedResult = 1)]
+        [TestCase(24, 24, 24, ExpectedResult = 24)]
+        [TestCase(-5, 10, 15, ExpectedResult = 5)]
+        [TestCase(5, 0, 2, ExpectedResult = 1)]
+        public int FindNod_SteinMethodForThree_CorrectInputValues_PositiveTest(int first, int second, int third)
+        {
+            return Nod.Stein(first, second, third);
+        }
+        #endregion
+
+        #region SteinForArray
+        [TestCase(new int[] { 5, 10, 15, 20 }, ExpectedResult = 5)]
+        [TestCase(new int[] { 24, 0, 24, -24 }, ExpectedResult = 24)]
+        [TestCase(new int[] { 5, -5, 15, 20 }, ExpectedResult = 5)]
+        public int FindNod_SteinMethodForArray_CorrectInputValues_PositiveTest(int[] array)
+        {
+            return Nod.Stein(array);
+        }
+        #endregion
+
+    }
+}
