1b72fa27750c6dcd2f9a97ce4cf6385b12a8e0d1
Unknown <dimidyuk9897@gmail.com>
2018-07-25 12:19:34 +0300

    Day14

---
+++ NET.S.2018.Dimidyuk.14/ClocksWithCountdown/CheckClocks.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using ClockLibrary;
+
+namespace Clocks
+{
+    class CheckClocks
+    {
+        static void Main(string[] args)
+        {
+            while (Console.ReadLine() != "stop")
+            {
+                Clock clock = new Clock();
+                Subscriber subscriber1 = new Subscriber { Name = "Kirill", Surname = "Dimidyuk", EMail = "kirill.dimidyuk@gmail.com" };
+
+                Subscriber subscriber2 = new Subscriber { Name = "Maxim", Surname = "Kondratovich", EMail = "maxim.Kondratovich@gmail.com" };
+
+                Subscriber subscriber3 = new Subscriber { Name = "Sasha", Surname = "Yachnik", EMail = "sasha.yachnik@gmail.com" };
+
+                clock.Message += subscriber1.Remember;
+
+                clock.Message += subscriber2.Remember;
+
+                clock.Message += subscriber3.Remember;
+
+                clock.StartEvent(2, "Life starts now");
+
+                clock.Message -= subscriber3.Remember;
+
+                clock.StartEvent(2, "Life starts now");
+            }
+        }
+    }
+}
---
+++ NET.S.2018.Dimidyuk.14/ClocksWithCountdown/Clock.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Threading;
+
+namespace ClockLibrary
+{
+    #region Clock
+    /// <summary>
+    /// class for timer
+    /// </summary>
+    public class Clock
+    {
+        static void Main(string[] args)
+        { }
+
+        public event EventHandler<MessageEventArgs> Message;
+
+        public Clock() { }
+
+        /// <summary>
+        /// Stops stream for some time
+        /// </summary>
+        /// <param name="time">time of pause of programm</param>
+        public void StartTimer(int time)
+        {
+            Thread.Sleep(time * 1000);
+        }
+
+        /// <summary>
+        /// Tells subscribers about events
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public static void EventStarted(object sender, MessageEventArgs e)
+        {
+            Console.WriteLine($"The event {e.message} is started.\n");
+        }
+
+        /// <summary>
+        /// Starst the event
+        /// </summary>
+        /// <param name="time"></param>
+        /// <param name="message"></param>
+        public void StartEvent(int time, string message)
+        {
+            StartTimer(time);
+
+            Subscription(new MessageEventArgs(time, message));
+        }
+
+        /// <summary>
+        /// Subscribes people to event
+        /// </summary>
+        /// <param name="messageEventArgs"></param>
+        private void Subscription(MessageEventArgs messageEventArgs)
+        {
+            Message.Invoke(this, messageEventArgs);
+        }
+    }
+    #endregion
+
+    public class MessageEventArgs
+    {
+        /// <summary>
+        /// Time before event
+        /// </summary>
+        public readonly int time;
+        
+        /// <summary>
+        /// Message of the event
+        /// </summary>
+        public readonly string message;
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="time">Time before event</param>
+        /// <param name="message">Message of the event</param>
+        public MessageEventArgs(int time, string message)
+        {
+            this.time = time;
+
+            this.message = message;
+        }
+    }
+
+    /// <summary>
+    /// Class of Subscribers
+    /// </summary>
+    public class Subscriber
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+    
+        public string EMail { get; set; }
+
+        /// <summary>
+        /// Remembers subscribers about event
+        /// </summary>
+        public void Remember(object sender, MessageEventArgs e)
+        {
+            Console.WriteLine($"{Surname} {Name}, you have subscribed for {e.message}");
+            Clock.EventStarted(sender, e);
+        }
+    }
+}
