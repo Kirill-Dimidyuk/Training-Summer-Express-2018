81df6fa86a32e3dc48139e5716811afbbf959b6b
Unknown <dimidyuk9897@gmail.com>
2018-07-29 11:21:22 +0300

    Day15

---
+++ NET.S.2018.Dimidyuk.15/GenericQueue/GenericQueue.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GenericQueue
+{
+    #region Queue
+    /// <summary>
+    /// class of Queue
+    /// </summary>
+    public class GenericQueue<T> : IEnumerable<T>
+    {
+        private T[] queue;
+        private int begin = 0;
+        private int end = -1;
+        private int count = 0;
+        private int capacity;
+        private const int defaultCapacity = 4;
+
+        #region Ctors
+        public GenericQueue() : this(defaultCapacity) { }
+
+        public GenericQueue(int capacity) 
+        {
+            if(capacity <= 0)
+            {
+                throw new ArgumentException($"{nameof(capacity)} must be grater than zero");
+            }
+
+            this.capacity = capacity;
+            queue = new T[capacity];
+        }
+        #endregion
+
+        #region Public Methods        
+        /// <summary>
+        /// Adds the specified object into a queue.
+        /// </summary>
+        /// <param name="obj">The object to be added</param>
+        public void Enqueue (T obj)
+        {
+            if((count + 1 > capacity))
+            {
+                ExtensionOfQueue();
+            }
+
+            end++;
+            count++;
+            queue[end] = obj;
+        }
+
+        /// <summary>
+        /// Deletes object from the queue
+        /// </summary>
+        /// <returns>deleted object</returns>
+        /// <exception cref="ArgumentException"></exception>
+        public T Dequeue()
+        {
+            if(count == 0)
+            {
+                throw new ArgumentException($"Queue is empty");
+            }
+
+            T item = queue[begin];
+
+            for (int i = 0; i < count - 1; i++)
+            {
+                queue[i] = queue[i + 1];
+            }
+
+            queue[count] = default(T);
+            count--;
+            end--;
+
+            return item;
+        }
+
+        /// <summary>
+        /// retures object fron the queue
+        /// </summary>
+        /// <returns>object fron the queue</returns>
+        /// <exception cref="ArgumentException"></exception>
+        public T Peek()
+        {
+            if (count == 0)
+            {
+                throw new ArgumentException($"Queue is empty");
+            }
+
+            return queue[begin];
+        }
+
+        /// <summary>
+        /// Makes the queue empty
+        /// </summary>
+        public void Clear()
+        {
+            queue = new T[defaultCapacity];
+            begin = 0;
+            end = -1;
+            count = 0;
+            capacity = defaultCapacity;
+        }
+        #endregion
+
+        #region Private Methods        
+        /// <summary>
+        /// Extends the queue.
+        /// </summary>
+        private void ExtensionOfQueue()
+        {
+            Array.Resize(ref queue, capacity * 2);
+            capacity *= 2;
+        }
+        #endregion
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            T[] copiedQueue = new T[capacity];
+            Array.Copy(queue, copiedQueue, copiedQueue.Length);
+            return new EnumeratorOfQueue<T>(copiedQueue, end, begin, count);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            T[] copiedQueue = new T[capacity];
+            Array.Copy(queue, copiedQueue, copiedQueue.Length);
+            return new EnumeratorOfQueue<T>(copiedQueue, end, begin, count);
+        }
+    }
+    #endregion
+
+    #region Enumerator
+    /// <summary>
+    /// Enumerator of queue
+    /// </summary>
+    public struct EnumeratorOfQueue<T> : IEnumerator<T>
+    {
+        private readonly T[] queue;
+        private readonly int end;
+        private readonly int begin;
+        private readonly int count;
+        private int currentIndex;
+        private int currentCount;
+
+        #region Ctor
+        public EnumeratorOfQueue(T[] queue, int end, int begin, int count)
+        {
+            if (queue is null)
+            {
+                throw new ArgumentNullException($"{nameof(queue)} can`t be null");
+            }
+
+            this.queue = queue;
+            this.end = end;
+            this.begin = begin;
+            this.count = count;
+            currentIndex =  begin - 1;
+            currentCount = 0;
+        }
+        #endregion
+
+        #region Methods of Enumerator
+        public T Current
+        {
+            get
+            {
+                if (currentIndex == -1 || currentIndex == count)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return queue[currentIndex];
+            }
+        }
+
+        object IEnumerator.Current => queue[currentIndex];
+
+        public void Dispose() { }
+
+        public bool MoveNext()
+        {
+            if (currentCount >= count)
+            {
+                return false;
+            }
+
+            if (currentIndex == queue.Length - 1)
+            {
+                currentIndex = -1;
+            }
+            currentIndex++;
+            currentCount++;
+            return true;
+        }
+
+        public void Reset()
+        {
+            currentIndex = begin;
+            currentCount = 0;
+        }
+    }
+    #endregion
+    #endregion
+}
---
+++ NET.S.2018.Dimidyuk.15/GenericQueue/GenericQueueTests.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using GenericQueue;
+
+namespace GenericQueue.Tests
+{
+    [TestClass]
+    public class GenericQueueTests
+    {
+        [Test]
+        public void GenericQueue_Enqueue_Success()
+        {
+            GenericQueue<int> queue = new GenericQueue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            queue.Enqueue(6);
+            int[] expected = { 1, 2, 3, 4, 5, 6 };
+
+            NUnit.Framework.CollectionAssert.AreEqual(queue, expected);
+        }
+
+        [Test]
+        public void GenericQueue_Dequeue_Success()
+        {
+            GenericQueue<string> queue = new GenericQueue<string>();
+            queue.Enqueue("a");
+            queue.Enqueue("b");
+            queue.Enqueue("c");
+            queue.Enqueue("d");
+            queue.Enqueue("e");
+            queue.Enqueue("f");
+            string dequeuedItem = queue.Dequeue();
+            string expected = "a";
+            string[] expected1 = {"b", "c", "d", "e", "f" };
+
+            NUnit.Framework.Assert.AreEqual(expected, dequeuedItem);
+
+            NUnit.Framework.CollectionAssert.AreEqual(queue, expected1);
+        }
+
+        [Test]
+        public void GenericQueue_Clear_Success()
+        {
+            GenericQueue<int> queue = new GenericQueue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            queue.Enqueue(6);
+            queue.Clear();
+            int[] expected = { } ;
+
+            NUnit.Framework.Assert.AreEqual(expected, queue);
+        }
+
+        [Test]
+        public void GenericQueue_Peek_Success()
+        {
+            GenericQueue<int> queue = new GenericQueue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            queue.Enqueue(6);
+            int[] expected = { 1, 2, 3, 4, 5, 6 };
+            int peekedItem = queue.Peek();
+            int expected1 = 1;
+
+            NUnit.Framework.Assert.AreEqual(expected1, peekedItem);
+
+            NUnit.Framework.CollectionAssert.AreEqual(queue, expected);
+        }
+    }
+}
